b5303fd116bcd979e433b131b9aac46e4ddbac6f
Natalyrubin <natirubrub@gmail.com>
2022-07-02 15:58:40 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,3 @@
+// For homework
+// let suits = ["&#9824", "&#9827", "&#9829;", "&#9830;"];
+// let suitNames = ["Spade", "Club", "Hart", "Diamond"];
---
+++ js/hm_cards.js
@@ -0,0 +1,14 @@
+
+
+
+
+let suitNames = ["Spade", "Club", "Heart", "Diamond"];
+
+
+for (let x = 0; x < suitNames.length; x++) {
+  for (let y = 1; y <= 13; y++) {
+    console.log(y.toString(), suitNames[x]);
+  }
+}
+
+
