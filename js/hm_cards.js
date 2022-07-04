081d0b84735096b63c81b1fe6d938791be4a5eed
Natalyrubin <natirubrub@gmail.com>
2022-07-04 18:23:38 +0300

    date 4.7

--- js/hm_cards.js
+++ js/hm_cards.js
@@ -1,13 +1,12 @@
 
 
 
-
 let suitNames = ["Spade", "Club", "Heart", "Diamond"];
 
 
 for (x = 0; x < suitNames.length; x++) {
   for (let y = 1; y <= 13; y++) {
-    console.log(y.toString(), suitNames[x]);
+    console.log(y.toString(),  suitNames[x]);
   }
 }
 
